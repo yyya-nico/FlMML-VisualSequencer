e8bb7517d5d4fdbfce9c72f31f24cd3f2640c686
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2024-10-22 04:01:54 +0900

    バージョンを転記

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+process.env.VITE_APP_VER = JSON.stringify(process.env.npm_package_version)
+    .replace(/["\-]|alpha|beta/g, match => ({
+        '"': '',
+        '-': ' ',
+        'alpha': 'Alpha',
+        'beta': 'Beta'
+    }[match]));
+
 export default {
     base: '/flmml-visualsequencer/'
 }
