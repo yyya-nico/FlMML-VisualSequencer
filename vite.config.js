99e10b935d5f1ecf584b9a20f4b5d3dfab3f4104
yyya_nico <30824964+yyya-nico@users.noreply.github.com>
2025-06-14 02:16:04 +0900

    謎のJSON.stringify()を除去、ビルド

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-process.env.VITE_APP_VER = JSON.stringify(process.env.npm_package_version)
-    .replace(/["\-]|alpha|beta/g, match => ({
-        '"': '',
+process.env.VITE_APP_VER = process.env.npm_package_version
+    .replace(/\-|alpha|beta/g, match => ({
         '-': ' ',
         'alpha': 'Alpha',
         'beta': 'Beta'
